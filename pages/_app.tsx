fff219aee75a419f829b8b371c8bb9660a066b73
Zofia Perzynska <zosiaperzynskaa@wp.pl>
2022-12-12 17:22:33 +0100

    Files adjusted

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<any>) {
+  return (
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
